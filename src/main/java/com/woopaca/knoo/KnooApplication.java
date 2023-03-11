e8ba9dfff7f29392be9df082b0ece052c5d217a0
지찬우 <jcw001031@gmail.com>
2023-03-11 10:56:57 +0900

    initial commit

---
+++ src/main/java/com/woopaca/knoo/KnooApplication.java
@@ -0,0 +1,13 @@
+package com.woopaca.knoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woopaca/knoo/KnooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woopaca.knoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
