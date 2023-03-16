a6e48a160900dcb62067d2430cad332d88e65617
지찬우 <jcw001031@gmail.com>
2023-03-16 19:23:22 +0900

    UserDetailsService 인터페이스 구현

---
+++ src/main/java/com/woopaca/knoo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.woopaca.knoo.service;
+
+import com.woopaca.knoo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+}
