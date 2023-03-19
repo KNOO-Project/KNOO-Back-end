9278819cd0eaaeed5ee8529cf38bf6b915e66b58
지찬우 <jcw001031@gmail.com>
2023-03-19 18:33:10 +0900

    모든 요청 허용

--- src/main/java/com/woopaca/knoo/config/security/SecurityConfig.java
+++ src/main/java/com/woopaca/knoo/config/security/SecurityConfig.java
@@ -28,11 +28,9 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 // permitAll() -> 모두 허용     authenticated() -> 인증 필요     hasRole() -> 권한 필요
-//                .antMatchers("/api/v1/auth/sign-up", "/api/v1/auth/sign-in").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/v1/posts").permitAll()
                 .antMatchers("/api/v1/auth/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
                 .and()
