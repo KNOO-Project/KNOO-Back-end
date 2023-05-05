c2e0f7b8908bfc39187baf96a98c314a3a65619e
jcw1031 <jcw001031@gmail.com>
2023-05-05 17:00:55 +0900

    이메일 메시지 생성 오류 코드 수정

--- src/main/java/com/woopaca/knoo/exception/mail/MailError.java
+++ src/main/java/com/woopaca/knoo/exception/mail/MailError.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum MailError implements KnooError {
 
-    MESSAGE_CAN_NOT_CREATE(HttpStatus.INTERNAL_SERVER_ERROR, "메일 메시지를 생성할 수 없습니다.", "KN");
+    MESSAGE_CAN_NOT_CREATE(HttpStatus.INTERNAL_SERVER_ERROR, "메일 메시지를 생성할 수 없습니다.", "KN501");
 
     private final HttpStatus httpStatus;
     private final String message;
