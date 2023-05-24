66d3ebd821c1d99ac0c50c78a7f42bd7daee08ee
jcw1031 <jcw001031@gmail.com>
2023-05-25 00:17:01 +0900

    @EnableWebSecurity 제거

--- src/main/java/com/woopaca/knoo/config/security/SecurityConfig.java
+++ src/main/java/com/woopaca/knoo/config/security/SecurityConfig.java
@@ -7,13 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
