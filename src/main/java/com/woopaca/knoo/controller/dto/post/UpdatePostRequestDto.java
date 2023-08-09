892d7ae14faabcd044ebb34a26ac16d0d5f1e3a8
jcw1031 <jcw001031@gmail.com>
2023-08-10 00:19:53 +0900

    fix: JsonAlias 설정

--- src/main/java/com/woopaca/knoo/controller/dto/post/UpdatePostRequestDto.java
+++ src/main/java/com/woopaca/knoo/controller/dto/post/UpdatePostRequestDto.java
@@ -1,5 +1,6 @@
 package com.woopaca.knoo.controller.dto.post;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,6 +25,7 @@ public class UpdatePostRequestDto {
     @Size(min = 2, max = 4000, message = "게시글 본문은 2자 이상, 4000자 이하이어야 합니다.")
     private String postContent;
 
+    @JsonAlias(value = "anonymous")
     @NotNull(message = "익명 선택 정보가 비어있습니다.")
     private Boolean isAnonymous;
 }
