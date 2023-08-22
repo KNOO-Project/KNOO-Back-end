f289f977e6f21a86ecbd0fed0c40b82f90793c78
jcw1031 <jcw001031@gmail.com>
2023-08-22 17:19:34 +0900

    fix: 검색 검증 추가

--- src/main/java/com/woopaca/knoo/controller/dto/post/PostSearchRequestDto.java
+++ src/main/java/com/woopaca/knoo/controller/dto/post/PostSearchRequestDto.java
@@ -4,6 +4,7 @@ import com.woopaca.knoo.entity.value.PostCategory;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
@@ -13,6 +14,7 @@ public class PostSearchRequestDto {
 
     private final PostCategory category;
     private final SearchCondition condition;
+    @NotBlank(message = "검색어는 비어있을 수 없습니다.")
     @Size(min = 2, message = "검색어는 2자 이상이어야 합니다.")
     private final String keyword;
     @Positive(message = "유효하지 않은 페이지입니다.")
